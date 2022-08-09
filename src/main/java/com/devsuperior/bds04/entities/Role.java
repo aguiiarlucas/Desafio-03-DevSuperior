73a319eb660c1b3b44c06ca646e71e4c69765ea4
Lucas Aguiar <lucascostaaguiar@hotmail.com>
2022-08-08 21:02:01 -0300

    Configuration

---
+++ src/main/java/com/devsuperior/bds04/entities/Role.java
@@ -0,0 +1,38 @@
+package com.devsuperior.bds04.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name="tb_role")
+public class Role implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String authority;
+
+    public Role() {
+    }
+
+    public Role(long id, String authority) {
+        this.id = id;
+        this.authority = authority;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+}
